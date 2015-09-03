4c0914e7a2374cb1b7ea49ed0076db1b6fef2dc5
Jean Pierre Charalambos <nakednous@gmail.com>
2015-09-03 15:52:05 -0500

    array.js added

---
+++ sketches/array.js
@@ -0,0 +1,37 @@
+// took from here http://p5js.org/examples/examples/Arrays_Array.php
+
+var coswave = [];
+
+function setup() {
+
+  createCanvas(720, 360);
+  for (var i = 0; i < width; i++) {
+    var amount = map(i, 0, width, 0, PI);
+    coswave[i] = abs(cos(amount));
+  }
+  background(255);
+  noLoop();
+}
+
+function draw() {
+  var y1 = 0;
+  var y2 = height/3;
+  for (var i = 0; i < width; i+=3) {
+    stroke(coswave[i]*255);
+    line(i, y1, i, y2);
+  }
+
+  y1 = y2;
+  y2 = y1 + y1;
+  for (var i = 0; i < width; i+=3) {
+    stroke(coswave[i]*255 / 4);
+    line(i, y1, i, y2);
+  }
+  
+  y1 = y2;
+  y2 = height;
+  for (var i = 0; i < width; i+=3) {
+    stroke(255 - coswave[i]*255);
+    line(i, y1, i, y2);
+  }
+}
